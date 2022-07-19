bea9126599aeac4e879598c8628486a5f5399ddf
radha123-g <109575939+radha123-g@users.noreply.github.com>
2022-07-19 13:56:12 +0530

    Update Practice1.java

--- Maven_Project/src/test/java/maven_Projects/testScripts/Practice1.java
+++ Maven_Project/src/test/java/maven_Projects/testScripts/Practice1.java
@@ -7,11 +7,7 @@ public void practice1() {
 	
 	System.out.println("Test1--> Test1");
 }
-@Test
-public void practic2() {
-	System.out.println("Test2--> Test2");
-	
-}
+
 @Test
 public void practice3() {
 	System.out.println("Test3--> Test3");
